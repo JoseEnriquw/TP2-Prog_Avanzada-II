6aaf5b13c2c2faddbf1d4fffc53290616f3dd617
Regina laurentino <reginalaurentino54@gmail.com>
2022-09-06 23:38:56 -0300

    holis

--- app/src/main/java/com/example/ejerciciocontroles_grupo3/ListadoContacto.java
+++ app/src/main/java/com/example/ejerciciocontroles_grupo3/ListadoContacto.java
@@ -2,10 +2,21 @@ package com.example.ejerciciocontroles_grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ListadoContacto extends AppCompatActivity {
+
+    private ListView Listvw;
+    private TextView tv1;
+
+    private String Listado [] = {"enrique", "regina", "laura", "ivan", "pablo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,32 @@ public class ListadoContacto extends AppCompatActivity {
         if(contacto!=null)
         Toast.makeText(this,contacto.toString(),Toast.LENGTH_LONG).show();
 
+        Listvw=(ListView)findViewById(R.id.ListView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, Listado);
+        Listvw.setAdapter(adapter);
+
+
+        Listvw.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+              String variable =  Listvw.getItemAtPosition(i).toString();
+
+                AbrirVentana(variable);
+
+
+            }
+
+        }
+    );
+
+
+
+    }
+
+
+    public void AbrirVentana(String nombre){
+        Intent masdetalles = new Intent(this, MasDetalles.class);
+        masdetalles.putExtra("name",nombre);
     }
 }
